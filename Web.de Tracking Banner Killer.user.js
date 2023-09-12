65043ed0964fbd59bb1cc7c6aa22bd936c66cb16
Jan <rsge@web.de>
2023-09-12 20:15:39 +0200

    Fixed Doge log

--- Web.de Tracking Banner Killer.user.js
+++ Web.de Tracking Banner Killer.user.js
@@ -5,7 +5,7 @@
 // @description    Removes the annoying "privacy settings" banner on German site Web.de's Freemail service asking you to accept all tracking, pay, or be bothered constantly.
 // @description:de Entfernt das nervige "Datenschutz"-Banner in Web.de Freemail, das euch zwingen will, zu bezahlen, jegliches Tracking zu akzeptieren oder ständig genervt zu werden.
 
-// @version        1.0.2
+// @version        1.0.3
 // @copyright      2023+, Jan G. (Rsge)
 // @license        Mozilla Public License 2.0
 // @icon           https://img.ui-portal.de/ux/webde/icons/favicon-32x32.png
@@ -26,12 +26,13 @@
   'use strict';
   var i;
   var dialogs = document.getElementsByClassName("dialog-app__blocker");
-  if (dialogs.length > 0) console.log("Killing:");
-  for (i = 0; i < dialogs.length; i++) {
+  var diaLen = dialogs.length;
+  if (diaLen > 0) console.log("Killing:");
+  for (i = 0; i < diaLen; i++) {
     console.log(dialogs[i]);
     dialogs[i].remove();
   }
-  if (dialogs.length > 0) {
+  if (diaLen > 0) {
     console.log(`
          ▄              ▄
         ▌▒█           ▄▀▒▌
