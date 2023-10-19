28fd485516b329cce7e6b83f1b3e0ebca9041140
Jan <rsge@web.de>
2023-10-19 08:18:43 +0200

    Added banner kill for Web.de base site

--- Web.de Tracking Banner Killer.user.js
+++ Web.de Tracking Banner Killer.user.js
@@ -5,7 +5,7 @@
 // @description    Removes the annoying "privacy settings" banner on German site Web.de's Freemail service asking you to accept all tracking, pay, or be bothered constantly.
 // @description:de Entfernt das nervige "Datenschutz"-Banner in Web.de Freemail, das euch zwingen will, zu bezahlen, jegliches Tracking zu akzeptieren oder ständig genervt zu werden.
 
-// @version        1.0.5
+// @version        1.1.0
 // @copyright      2023+, Jan G. (Rsge)
 // @license        Mozilla Public License 2.0
 // @icon           https://img.ui-portal.de/ux/webde/icons/favicon-32x32.png
@@ -16,6 +16,7 @@
 // @updateURL      https://greasyfork.org/scripts/475107-web-de-tracking-banner-killer/code/Webde%20Tracking%20Banner%20Killer.user.js
 // @downloadURL    https://greasyfork.org/scripts/475107-web-de-tracking-banner-killer/code/Webde%20Tracking%20Banner%20Killer.user.js
 
+// @match          https://web.de/*
 // @match          https://navigator.web.de/*
 // @match          https://bap.navigator.web.de/*
 
@@ -25,15 +26,25 @@
 
 (function() {
   'use strict';
-  let i;
-  let dialogs = document.getElementsByClassName("dialog-app__blocker");
-  let diaLen = dialogs.length;
-  if (diaLen > 0) console.log("Killing:");
-  for (i = 0; i < diaLen; i++) {
-    console.log(dialogs[i]);
-    dialogs[i].remove();
+  let blocked = false;
+  let redirect = false;
+  if(document.URL.includes("consent-management")) {
+    blocked = true;
+    redirect = true;
+  } else {
+      let i;
+      let dialogs = document.getElementsByClassName("dialog-app__blocker");
+      let diaLen = dialogs.length
+      if (diaLen > 0) {
+        console.log("Killing:");
+        blocked = true;
+      }
+      for (i = 0; i < diaLen; i++) {
+          console.log(dialogs[i]);
+          dialogs[i].remove();
+      }
   }
-  if (diaLen > 0) {
+  if (blocked) {
     console.log(`
          ▄              ▄
         ▌▒█           ▄▀▒▌
@@ -54,5 +65,8 @@
     ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀
    ▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀
   ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀    https://github.com/Rsge/Web.de-Tracking-Banner-Killer`);
+  }
+  if (redirect) {
+    window.open("https://web.de/", "_top");
   }
 })();
